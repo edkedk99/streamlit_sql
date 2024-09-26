00b3d787f4d1cbad74b9017f745d5ef1d73aad85
Eduardo <32989294+edkedk99@users.noreply.github.com>
2024-09-26 18:42:04 -0300

    First version

---
+++ streamlit_sql/__init__.py
@@ -0,0 +1,4 @@
+from .sql_ui import show_page
+from .update_model import show_create, show_update
+
+__all__ = ["show_page", "show_create", "show_update"]
--- streamlit_sql/__main__.py
+++ streamlit_sql/__main__.py
@@ -1,5 +1,2 @@
-
-
-
 def main():
     pass
